87ccad050c898d6d5f792f3912e39e3cb28d80d4
Sherest Mehta <shmehta@adobe.com>
2024-03-21 16:07:38 +0530

    updated with WKND source

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+// Drop-in Providers
+import { render as cartProvider } from '@dropins/storefront-cart/render.js';
+
+// Drop-in Containers
+import MiniCart from '@dropins/storefront-cart/containers/MiniCart.js';
+
+// Drop-in Tools
+import { events } from '@dropins/tools/event-bus.js';
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
@@ -86,6 +96,47 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
+function addAnimation() {
+  window.addEventListener('scroll', () => {
+    const header = document.getElementsByClassName('header-nav-wrapper')[0];
+    const scrollPosition = window.scrollY;
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth > 900) {
+      if (scrollPosition > 168) {
+        header.classList.add('minimized');
+      } else {
+        header.classList.remove('minimized');
+      }
+    } else {
+      header.classList.remove('minimized');
+    }
+  });
+}
+
+function setActiveTab() {
+  const currentPath = window.location.pathname;
+  const matchResult = currentPath.match(/^\/([^/]+)/);
+  const path = matchResult ? matchResult[1] : null;
+  const navTabLinks = document.querySelector('.nav-sections ul');
+
+  [...navTabLinks.children].forEach((tab) => {
+    const link = tab.querySelector('a');
+    const linkTitle = link.title.toLowerCase();
+
+    if (linkTitle === path || (linkTitle === 'shop' && ['products', 'equipment', 'search'].includes(path))) {
+      link.classList.add('active');
+    }
+  });
+
+  /* temp - only for the demo since the adventures landing page is the "home page"
+  */
+  if (!path) {
+    const adventureTab = navTabLinks.querySelector('a[title="Adventures"],a[title="adventures"]');
+    adventureTab.classList.add('active');
+  }
+}
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -128,6 +179,93 @@ export default async function decorate(block) {
     });
   }
 
+  const navTools = nav.querySelector('.nav-tools');
+
+  /** Mini Cart */
+  const minicart = document.createRange().createContextualFragment(`
+    <div class="minicart-wrapper">
+      <button type="button" class="button nav-cart-button"></button>
+      <div class="minicart-panel nav-panel"></div>
+    </div>
+  `);
+
+  navTools.append(minicart);
+
+  const minicartPanel = navTools.querySelector('.minicart-panel');
+
+  const cartButton = navTools.querySelector('.nav-cart-button');
+  cartButton.setAttribute('aria-label', 'Cart');
+
+  async function toggleMiniCart(state) {
+    const show = state ?? !minicartPanel.classList.contains('nav-panel--show');
+
+    if (show) {
+      await cartProvider.render(MiniCart, {
+        routeEmptyCartCTA: () => '/',
+        routeProduct: (product) => `/products/${product.url.urlKey}/${product.sku}`,
+        routeCart: () => '/cart',
+        routeCheckout: () => '/checkout',
+      })(minicartPanel);
+    } else {
+      minicartPanel.innerHTML = '';
+    }
+
+    minicartPanel.classList.toggle('nav-panel--show', show);
+  }
+
+  cartButton.addEventListener('click', () => toggleMiniCart());
+
+  // Cart Item Counter
+  events.on('cart/data', (data) => {
+    if (data?.totalQuantity) {
+      cartButton.setAttribute('data-count', data.totalQuantity);
+    } else {
+      cartButton.removeAttribute('data-count');
+    }
+  }, { eager: true });
+
+  /** Search */
+  const search = document.createRange().createContextualFragment(`
+  <div class="search-wrapper">
+    <button type="button" class="button nav-search-button">Search</button>
+    <div class="nav-search-input nav-search-panel nav-panel hidden">
+      <form id="search_mini_form" action="/search" method="GET">
+        <input id="search" type="search" name="q" placeholder="Search" />
+        <div id="search_autocomplete" class="search-autocomplete"></div>
+      </form>
+    </div>
+  </div>
+  `);
+
+  navTools.append(search);
+
+  const searchPanel = navTools.querySelector('.nav-search-panel');
+  const searchButton = navTools.querySelector('.nav-search-button');
+  const searchInput = searchPanel.querySelector('input');
+
+  function toggleSearch(state) {
+    const show = state ?? !searchPanel.classList.contains('nav-panel--show');
+    searchPanel.classList.toggle('nav-panel--show', show);
+    if (show) searchInput.focus();
+  }
+
+  navTools.querySelector('.nav-search-button').addEventListener('click', async () => {
+    await import('./searchbar.js');
+    document.querySelector('header .nav-search-input').classList.toggle('hidden');
+    toggleSearch();
+  });
+
+  // Close panels when clicking outside
+  document.addEventListener('click', (e) => {
+    if (!minicartPanel.contains(e.target) && !cartButton.contains(e.target)) {
+      toggleMiniCart(false);
+    }
+
+    if (!searchPanel.contains(e.target) && !searchButton.contains(e.target)) {
+      toggleSearch(false);
+    }
+  });
+
   // hamburger for mobile
   const hamburger = document.createElement('div');
   hamburger.classList.add('nav-hamburger');
@@ -142,7 +280,10 @@ export default async function decorate(block) {
   isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
 
   const navWrapper = document.createElement('div');
-  navWrapper.className = 'nav-wrapper';
+  navWrapper.className = 'header-nav-wrapper';
   navWrapper.append(nav);
   block.append(navWrapper);
+
+  addAnimation();
+  setActiveTab();
 }
